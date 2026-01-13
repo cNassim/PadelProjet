a24cb02fd6762f31c0b9fc627bb4834e1e5c6dec
Sara Dahhoumi <sara.dahhoumi111@gmail.com>
2026-01-13 09:18:59 +0100

    Tests à faire en commentaire

--- backend/tests/test_player.py
+++ backend/tests/test_player.py
@@ -1,4 +1,15 @@
 # ============================================
-# FICHIER : backend/tests/test_player.py
+# FICHIER : TEST INTEGRATION  backend/tests/test_player.py
 # ============================================
 
+#test_create_player_success
+#test_update_player_sucess
+#test_update_licence_fail
+
+#test_delete_player_in_active_team_fails
+#test_delete_player_no_team_success
+#test_create_player_invalid_license
+#test_create_player_future_birthdate
+#test_create_player_duplicate_licence
+
+ 
